9be92ecb33a204453275a5ccd4bc476dc6f096a3
Edith <eddith@proton.me>
2025-04-26 12:36:12 +0200

    feat: get to resize borad and timelines

--- src/modules/board/index.svelte
+++ src/modules/board/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<div class="flex h-screen w-full flex-col bg-gray-900 p-2">
+<div class="flex h-screen flex-col bg-gray-900 p-2">
 	<p class="py-3 text-xs">Buttons board</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,36 @@
 	import VideoPlayer from '../modules/videoplayer/index.svelte';
 	import Board from '../modules/board/index.svelte';
 	import ProjectStore from '../stores/project.svelte';
+
+	let leftWidth = 50;
+	let isResizing = false;
+
+	function startResize(event: MouseEvent) {
+		isResizing = true;
+		document.addEventListener('mousemove', resize);
+		document.addEventListener('mouseup', stopResize);
+	}
+
+	function resize(event: MouseEvent) {
+		if (isResizing) {
+			const totalWidth = window.innerWidth;
+			leftWidth = Math.min(70, Math.max(20, (event.clientX / totalWidth) * 100)); // Clamp between 20% and 80%
+		}
+	}
+
+	function stopResize() {
+		isResizing = false;
+		document.removeEventListener('mousemove', resize);
+		document.removeEventListener('mouseup', stopResize);
+	}
 </script>
 
-<div class="flex w-full flex-row gap-3">
-	<div class="w-1/2">
+<div class="flex w-full flex-row gap-1">
+	<div class="flex-shrink-0" style="width: {leftWidth}%">
 		<VideoPlayer video={ProjectStore.getProject().video?.path} />
 	</div>
-	<div class="w-1/2">
+	<div class="w-1 cursor-col-resize bg-gray-900" on:mousedown={startResize}></div>
+	<div class="flex-grow">
 		<Board />
 	</div>
 </div>
