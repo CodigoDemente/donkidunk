10e1a6650118628ff0765a73ed39dcd8cb18a2dc
Ale <alesanchezmedina@gmail.com>
2025-05-03 09:06:14 +0200

    feat: :sparkles: Add SQL dependency

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ pub fn run() {
     env::set_var("FFMPEG_PATH", ffmpeg_path.unwrap().to_str().unwrap());
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
