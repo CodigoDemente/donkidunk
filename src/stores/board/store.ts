2548d712a4c1616b67f66aa8644a3f018b707ff6
Edith <eddith@proton.me>
2025-05-22 15:59:18 +0200

    feat: actions and stores

--- src/stores/board/store.ts
+++ src/stores/board/store.ts
@@ -1,72 +1,173 @@
 import { writable } from 'svelte/store';
 
-export interface EventCategory {
+export interface Category {
+	id: string;
 	name: string;
 	color: string;
+	onGrid: number[];
+	buttons: Action[];
 }
-export interface ActionCategory {
-	name: string;
-	color: string;
-}
+
 export interface Action {
+	id: string;
 	name: string;
+}
+
+export interface Tag {
+	id: string;
+	name: string;
+	color: string;
 }
 export interface BoardStore {
-	eventCategories: Record<string, EventCategory>;
-	actionCategories: Record<string, ActionCategory>;
-	actions: Record<string, Action>;
+	isEditing: boolean;
+	eventCategories: Category[];
+	actionCategories: Category[];
+	tagsRelatedToEvents: Tag[];
 }
 
 const initialState: BoardStore = {
-	eventCategories: {
-		'1': {
-			name: 'Attack',
+	isEditing: false,
+	eventCategories: [
+		{
+			id: '1',
+			name: '',
+			color: '#FFDD00',
+			onGrid: [1, 1],
+			buttons: [
+				{
+					id: '1-1',
+					name: 'Attack'
+				}
+			]
+		},
+		{
+			id: '2',
+			name: '',
+			color: '#FF0000',
+			onGrid: [3, 4],
+			buttons: [
+				{
+					id: '2-1',
+					name: 'Defense'
+				}
+			]
+		}
+	],
+	tagsRelatedToEvents: [
+		{
+			id: '#4',
+			name: 'Whatever',
 			color: '#FFDD00'
 		},
-		'2': {
-			name: 'Defense',
+		{
+			id: '#5',
+			name: 'Whatever3',
+			color: '#FF0000'
+		},
+		{
+			id: '#6',
+			name: 'Whatever4',
 			color: '#FF0000'
 		}
-	},
-	actionCategories: {
-		'13': {
+	],
+	actionCategories: [
+		{
+			id: '13',
 			name: 'Shoots',
-			color: '#924F97'
+			color: '#924F97',
+			onGrid: [5, 6],
+			buttons: [
+				{
+					id: '13-1',
+					name: 'One point shoot'
+				},
+				{
+					id: '13-2',
+					name: 'Two point shoot'
+				},
+				{
+					id: '13-3',
+					name: 'Three point shoot'
+				}
+			]
 		},
-		'14': {
+		{
+			id: '14',
 			name: 'System',
-			color: '#FF5009'
+			color: '#FF5009',
+			onGrid: [7, 8],
+			buttons: [
+				{
+					id: '14-5',
+					name: 'System A'
+				},
+				{
+					id: '14-6',
+					name: 'System B'
+				},
+				{
+					id: '14-7',
+					name: 'System C'
+				}
+			]
 		},
-		'15': {
+		{
+			id: '15',
 			name: 'Defense Systems',
-			color: '#3357FF'
+			color: '#3357FF',
+			onGrid: [9, 10],
+			buttons: [
+				{
+					id: '15-1',
+					name: 'Block'
+				},
+				{
+					id: '15-2',
+					name: 'Screen'
+				},
+				{
+					id: '15-3',
+					name: 'Player 3'
+				}
+			]
 		},
-		'16': {
+		{
+			id: '16',
 			name: 'Other Category',
-			color: '#FF33A1'
+			color: '#FF33A1',
+			onGrid: [11, 4],
+			buttons: [
+				{
+					id: '16-1',
+					name: 'Player 1'
+				},
+				{
+					id: '16-2',
+					name: 'Player 2'
+				},
+				{
+					id: '16-3',
+					name: 'Player 3'
+				}
+			]
 		},
-		'17': {
+		{
+			id: '17',
 			name: 'Techniques',
-			color: '#FF8C33'
+			color: '#FF8C33',
+			onGrid: [12, 3],
+			buttons: [
+				{
+					id: '17-1',
+					name: 'Fast break'
+				},
+				{
+					id: '17-2',
+					name: 'Isolation'
+				}
+			]
 		}
-	},
-	actions: {
-		'13-1': { name: 'One point shoot' },
-		'13-2': { name: 'Two point shoot' },
-		'13-3': { name: 'Three point shoot' },
-		'14-5': { name: 'System A' },
-		'14-6': { name: 'System B' },
-		'14-7': { name: 'System C' },
-		'15-1': { name: 'Block' },
-		'15-2': { name: 'Screen' },
-		'15-3': { name: 'Player 3' },
-		'16-1': { name: 'Player 1' },
-		'16-2': { name: 'Player 2' },
-		'16-3': { name: 'Player 3' },
-		'17-1': { name: 'Fast break' },
-		'17-2': { name: 'Isolation' },
-		'17-3': { name: 'Rebound' }
-	}
+	]
 };
 
 export const boardStore = writable<BoardStore>(initialState);
