afa04d9c51e9c7984f61eee3c9e29b3c5a2c1c4c
Ale Sanchez <alesanchezmedina@gmail.com>
2025-07-10 20:28:45 +0200

    3 cortar video ffmpeg (#36)
    * feat: :sparkles: Try to play video on app
    
    * fix: :wrench: Enable assets protocol
    
    * feat: cut_video function call
    
    * feat(ffmpeg): :recycle: Use select filter instead of several cuts
    
    * feat(ffmpeg): :recycle: Use precompiled static binaries instead of ffmpeg-next
    
    * feat: :bricks: Add executables to git lfs
    
    * refactor: :recycle: Refactor structure to allow app testing
    
    * feat(menu): :white_check_mark: Add tests
    
    * fix: :rotating_light: Clippy errors
    
    * fix: :bug: Several fixes
    
    * feat: :green_heart: Refactor CI build to use cache and make parallel jobs
    
    * fix: :green_heart: Add PNPM to workflow
    
    * fix: :green_heart: Use latest version of cache action
    
    * refactor: :green_heart: Move stages around
    
    * fix: :green_heart: Upgrade action version
    
    * fix: :green_heart: Specify subdirectory for cache
    
    ---------
    
    Co-authored-by: Edith <eddith@proton.me>

---
+++ src-tauri/src/commands/menu.rs
@@ -0,0 +1,59 @@
+use crate::menu::MenuExtensions;
+
+#[tauri::command]
+pub fn set_menu_item_enabling_status<R: tauri::Runtime>(
+    app: tauri::AppHandle<R>,
+    menu_id: &str,
+    enabled: bool,
+) -> Result<(), String> {
+    let menu = app.menu();
+
+    if let Some(menu) = menu {
+        let menu_id = tauri::menu::MenuId::new(menu_id);
+        menu.set_enabled_by_item_id(&menu_id, enabled)
+            .map_err(|e| e.to_string())?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod enable_item {
+        use super::*;
+
+        #[test]
+        #[cfg_attr(target_os = "macos", ignore)] // macOS menu can only be created in the main thread
+        fn should_enable_item_by_id() {
+            let app = tauri::test::mock_app();
+            let mock_id = "test_menu";
+            let mock_text = "Test Menu";
+
+            let mock_menu = tauri::menu::Menu::new(app.handle()).unwrap();
+
+            let menu_item = tauri::menu::MenuItemBuilder::with_id(mock_id, mock_text)
+                .build(app.handle())
+                .unwrap();
+
+            mock_menu.append(&menu_item).unwrap();
+
+            app.set_menu(mock_menu).unwrap();
+
+            set_menu_item_enabling_status(app.handle().to_owned(), mock_id, false).unwrap();
+
+            assert!(!menu_item.is_enabled().unwrap());
+        }
+
+        #[test]
+        fn should_return_ok_if_menu_not_found() {
+            let app = tauri::test::mock_app();
+            let menu_id = "non_existent_menu";
+
+            let result = set_menu_item_enabling_status(app.handle().to_owned(), menu_id, true);
+
+            assert!(result.is_ok());
+        }
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,91 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    donkidunk_lib::run()
+mod commands;
+mod menu;
+#[cfg(target_os = "linux")]
+mod server;
+
+use commands::menu::*;
+use commands::video::*;
+
+fn create_app<R: tauri::Runtime>(builder: tauri::Builder<R>) -> tauri::App<R> {
+    builder
+        .invoke_handler(tauri::generate_handler![
+            #[cfg(target_os = "linux")]
+            server::get_linux_file_url,
+            set_menu_item_enabling_status,
+            cut_video,
+        ])
+        .setup(|app| {
+            #[cfg(target_os = "linux")]
+            tokio::spawn(server::setup_webserver());
+
+            menu::setup_menu(app)?;
+
+            Ok(())
+        })
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+}
+
+#[tokio::main]
+async fn main() {
+    let builder = tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_sql::Builder::default().build());
+
+    let app = create_app(builder);
+
+    app.run(|app_handle, event| {
+        log::debug!("Tauri event: {event:?}");
+
+        match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                log::debug!("Exit requested, exiting...");
+                api.prevent_exit();
+                app_handle.exit(0);
+            }
+            tauri::RunEvent::Ready => {
+                log::debug!("Tauri is ready");
+            }
+            _ => {}
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use std::process::Command;
+
+    use super::*;
+    use tauri::{path, Manager};
+    use tauri_plugin_shell::ShellExt;
+
+    #[ignore]
+    #[tokio::test]
+    async fn it_should_run_ffmpeg_command() -> Result<(), String> {
+        let builder = tauri::test::mock_builder().plugin(tauri_plugin_shell::init());
+        let app = create_app(builder);
+
+        let path = path::PathResolver::app_data_dir(&app.path()).unwrap();
+
+        println!("App data path: {:?}", path);
+
+        let ffmpeg = app.shell().sidecar("donkidunk_ffmpeg").unwrap();
+
+        let status = Command::from(ffmpeg)
+            .arg("-version")
+            .status()
+            .map_err(|e| e.to_string())?;
+
+        assert!(status.success());
+
+        Ok(())
+    }
 }
