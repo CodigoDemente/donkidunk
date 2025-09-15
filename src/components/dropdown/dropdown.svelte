e3c5d2531a7e347a51b760461ea25f69e271c829
Edith <eddith@proton.me>
2025-09-15 12:39:53 +0200

    feat: dropdown and input components

--- src/components/dropdown/dropdown.svelte
+++ src/components/dropdown/dropdown.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { IconChevronDown } from '@tabler/icons-svelte';
+	import type { InputSizes } from '../input/types';
 
 	export let label: string = '';
-	export let options: { value: string; label: string }[] = [];
+	export let options: { value: string | number; label: string }[] = [];
 	export let value: string = '';
 	export let name: string = '';
 	export let id: string = '';
@@ -11,11 +12,13 @@
 	export let labelClass: string = '';
 	export let selectClass: string = '';
 	export let error: string = '';
-	export let size: 'small' | 'medium' | 'large' | 'full' = 'medium';
+	export let size: InputSizes = 'medium';
 	export let horizontal: boolean = false;
-	export let onChange: (e: Event) => void = () => {};
+	export let placeholder: string | undefined = undefined;
+	export let noErrors: boolean = false; // Prop that controls error space
 
 	const sizeToClass = {
+		mini: 'w-18',
 		small: 'w-28',
 		medium: 'w-46',
 		large: 'w-72',
@@ -24,20 +27,19 @@
 
 	let open = false;
 
-	function selectOption(val: string) {
-		value = val;
-		onChange(new CustomEvent('change', { detail: val }));
+	const onChange = (val: string | number) => {
+		value = val as string;
 		open = false;
-	}
+	};
 </script>
 
 <label
-	class={`mb-2 flex ${horizontal ? 'flex-row items-start' : 'flex-col items-start gap-2'} text-sm text-white ${labelClass}`}
+	class={`flex ${horizontal ? 'flex-row items-start' : 'flex-col items-start gap-2'} text-sm text-white ${labelClass}`}
 >
 	{#if label}
 		<p class={`text-sm ${horizontal ? ' w-[130px]' : ''}`}>{label}</p>
 	{/if}
-	<div class="relative flex h-[48px] w-full flex-col">
+	<div class="relative flex {!noErrors && `h-[48px]`} w-full flex-col">
 		<button
 			type="button"
 			class={`h-[26px] rounded border-0 bg-gray-700 px-2 py-1 text-left text-sm text-white ${sizeToClass[size]} ${selectClass} flex items-center justify-between transition-all
@@ -52,7 +54,7 @@
 			{name}
 		>
 			<span class={value ? 'leading-4' : 'w-full text-xs text-gray-400'}>
-				{options.find((o) => o.value === value)?.label ?? 'Select...'}
+				{options.find((o) => o.value === value)?.label ?? placeholder}
 			</span>
 			<IconChevronDown
 				class="ml-auto p-1 transition-transform duration-200"
@@ -72,7 +74,7 @@
 						class="cursor-pointer px-2 py-1 text-white hover:bg-gray-600"
 						role="option"
 						aria-selected={opt.value === value}
-						on:click={() => selectOption(opt.value)}
+						on:mousedown|preventDefault={() => onChange(opt.value)}
 					>
 						{opt.label}
 					</li>
--- src/components/input/input.svelte
+++ src/components/input/input.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { InputSizes } from './types';
+
 	export let label: string = '';
 	export let value: string = '';
 	export let type: string = 'text';
@@ -9,18 +11,20 @@
 	export let disabled: boolean = false;
 	export let autocomplete: string = 'off';
 	export let readonly: boolean = false;
-	export let min: string | number = '';
-	export let max: string | number = '';
+	export let minlength: number | undefined = undefined;
+	export let maxlength: number | undefined = undefined;
 	export let step: string | number = '';
 	export let pattern: string = '';
 	export let inputClass: string = '';
 	export let labelClass: string = '';
 	export let error: string = '';
-	export let size: 'small' | 'medium' | 'large' | 'full' = 'medium';
+	export let size: InputSizes = 'medium';
 	export let horizontal: boolean = false;
 	export let onInput: (e: Event) => void = () => {};
+	export let noErrors: boolean = false; // If passed no fixed space for errors will be reserved
 
 	const sizeToClass = {
+		mini: 'w-18',
 		small: 'w-28',
 		medium: 'w-46',
 		large: 'w-72',
@@ -29,12 +33,12 @@
 </script>
 
 <label
-	class={`mb-2 flex ${horizontal ? 'flex-row items-start' : 'flex-col items-start gap-2'} text-sm text-white ${labelClass}`}
+	class={`flex ${horizontal ? 'flex-row items-start' : 'flex-col items-start gap-2'} text-sm text-white ${labelClass}`}
 >
 	{#if label}
 		<p class={`text-sm ${horizontal ? ' w-[130px]' : ''}`}>{label}</p>
 	{/if}
-	<div class="flex h-[48px] w-full flex-col">
+	<div class="flex {!noErrors && `h-[48px]`} w-full flex-col">
 		<input
 			{id}
 			class={`rounded bg-gray-700 px-2 py-1 text-sm text-white placeholder:text-xs placeholder:text-gray-400 ${sizeToClass[size]} ${inputClass}`}
@@ -46,8 +50,8 @@
 			{disabled}
 			autocomplete={autocomplete as any}
 			{readonly}
-			{min}
-			{max}
+			{minlength}
+			{maxlength}
 			{step}
 			{pattern}
 			on:input={onInput}
