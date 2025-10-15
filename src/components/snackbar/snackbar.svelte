26a8628d38f2ed6823af0acca8ceb24a259c1a09
Edith <eddith@proton.me>
2025-10-15 19:26:54 +0200

    feat: rearrange snackbar style

--- src/components/snackbar/snackbar.svelte
+++ src/components/snackbar/snackbar.svelte
@@ -21,12 +21,11 @@
 		projectActions.hideSnackbar();
 	}
 
-	// Color palette for backgrounds and text
 	const bgColors = {
-		error: 'bg-red-200',
-		info: 'bg-blue-200',
-		success: 'bg-green-200',
-		warning: 'bg-yellow-200'
+		error: 'bg-red-200/90',
+		info: 'bg-blue-200/90',
+		success: 'bg-green-200/90',
+		warning: 'bg-yellow-200/90'
 	};
 	const textColors = {
 		error: 'text-red-800',
@@ -39,10 +38,9 @@
 {#if snackbarStore.show}
 	<div
 		class={`fixed top-0 left-1/2 z-80 mt-6 flex max-w-[90vw] min-w-[280px] -translate-x-1/2 items-start rounded-sm px-6 py-4
-            ${bgColors[snackbarStore.type] ?? 'bg-gray-100'}
-            shadow-md
-        `}
-		style="box-shadow: 0 6px 32px 0 rgba(0,0,0,0.10);"
+				${bgColors[snackbarStore.type] ?? 'bg-gray-100/80'}
+				border border-${bgColors[snackbarStore.type]} shadow-2xl backdrop-blur-sm
+			`}
 	>
 		<div class="flex flex-1 flex-col gap-1 pr-6">
 			{#if snackbarStore.title}
@@ -54,14 +52,13 @@
 				<p class={`text-sm ${textColors[snackbarStore.type]}`}>{snackbarStore.message}</p>
 			{/if}
 		</div>
-		{#if snackbarStore.mode === 'manual'}
-			<button
-				class="hover:gray-800 absolute top-2 right-3 text-xl font-bold text-gray-500 transition-opacity hover:cursor-pointer"
-				onclick={close}
-				aria-label="Close"
-			>
-				&times;
-			</button>
-		{/if}
+
+		<button
+			class="hover:gray-800 absolute top-2 right-3 text-xl font-bold text-gray-500 transition-opacity hover:cursor-pointer"
+			onclick={close}
+			aria-label="Close"
+		>
+			&times;
+		</button>
 	</div>
 {/if}
