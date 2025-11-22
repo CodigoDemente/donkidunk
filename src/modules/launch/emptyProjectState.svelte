2c23b319ad92b0c29ed8117f1cae546309572408
Edith <eddith@proton.me>
2025-11-22 22:49:59 +0100

    feat: arrange styles

--- src/modules/launch/emptyProjectState.svelte
+++ src/modules/launch/emptyProjectState.svelte
@@ -49,11 +49,15 @@
 	}
 </script>
 
-<div class="flex h-full flex-col items-center justify-center gap-4">
-	<h2 class="text-2xl font-semibold text-gray-300">No Project Loaded</h2>
-	<p class="text-gray-400">Create a new project or open an existing one to get started</p>
-	<div class="flex gap-4">
-		<Button onClick={handleCreateProject} primary>Create New Project</Button>
+<div
+	class="flex h-full flex-col items-center justify-center gap-12 rounded-lg border border-gray-600 bg-gray-800"
+>
+	<div class="flex items-center gap-4">
+		<Button tertiary onClick={handleCreateProject}>New Project</Button>
+		or
 		<Button onClick={handleOpenProject}>Open Project</Button>
 	</div>
+	<p class="w-2/4 text-center text-lg text-gray-400">
+		Create a new project or open an existing one to get started
+	</p>
 </div>
--- src/modules/modalContent/addNewProject/index.svelte
+++ src/modules/modalContent/addNewProject/index.svelte
@@ -33,40 +33,35 @@
 	}
 </script>
 
-<div class="flex flex-1 flex-col gap-4 p-4">
-	<!-- Project Path -->
-	<div class="flex flex-col gap-2">
-		<div class="flex items-end gap-2">
-			<Input
-				horizontal
-				label="Project Path"
-				placeholder="Select where to save the project"
-				type="text"
-				readonly
-				bind:value={projectPath}
-				size="full"
-				inputClass="bg-gray-700"
-				noErrors
-			/>
-			<Button onClick={handleSelectProjectPath} size="medium">Browse</Button>
-		</div>
+<div class="flex w-full flex-col gap-10 px-4 pt-4 pb-8">
+	<p class="border-b border-gray-500 text-sm text-gray-200">
+		To create a new project, indicate where you will save the file and the video to import:
+	</p>
+	<div class="flex w-full items-end gap-2">
+		<Input
+			label="Project path"
+			placeholder="Select where to save the project..."
+			type="text"
+			readonly
+			bind:value={projectPath}
+			size="large"
+			inputClass="bg-gray-700"
+			noErrors
+		/>
+		<Button tertiary onClick={handleSelectProjectPath} size="small">Browse</Button>
 	</div>
 
-	<!-- Video File -->
-	<div class="flex flex-col gap-2">
-		<div class="flex items-end gap-2">
-			<Input
-				horizontal
-				label="Video File"
-				placeholder="Select video file to import"
-				type="text"
-				readonly
-				bind:value={videoPath}
-				size="full"
-				inputClass="bg-gray-700"
-				noErrors
-			/>
-			<Button onClick={handleSelectVideoFile} size="medium">Browse</Button>
-		</div>
+	<div class="flex items-end gap-2">
+		<Input
+			label="Video file"
+			placeholder="Select video file to import..."
+			type="text"
+			readonly
+			bind:value={videoPath}
+			size="large"
+			inputClass="bg-gray-700"
+			noErrors
+		/>
+		<Button tertiary onClick={handleSelectVideoFile} size="small">Browse</Button>
 	</div>
 </div>
