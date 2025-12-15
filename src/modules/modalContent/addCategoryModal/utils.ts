5e16dbfc49583572bb2358998952e61a29dc34fa
Edith <eddith@proton.me>
2025-12-15 10:26:30 +0100

    feat: change dropdown values for duration and before

--- src/modules/modalContent/addCategoryModal/utils.ts
+++ src/modules/modalContent/addCategoryModal/utils.ts
@@ -11,7 +11,32 @@ export const typeOptions: Option[] = [
 	}
 ];
 
-export const secondsOptions: Option[] = [null, 1, 2, 3, 4, 5].map((num) => {
+export const secondsDurationOptions: Option[] = [
+	null,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20
+].map((num) => {
+	return {
+		value: num,
+		label: `${num ? num + 's' : '-'}`
+	};
+});
+
+export const secondsBeforeOptions: Option[] = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => {
 	return {
 		value: num,
 		label: `${num ? num + 's' : '-'}`
