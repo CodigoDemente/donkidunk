991b9576e33bf688d75e9b635c328416260d3713
Edith <eddith@proton.me>
2025-12-26 23:46:22 +0100

    feat: delete event logic

---
+++ src/modules/modalContent/deleteEventModal/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<p class="p-4 text-gray-100">
+	You are to remove this event from the timeline. <br />
+	<strong>Are you sure you want to delete this event?</strong>
+</p>
