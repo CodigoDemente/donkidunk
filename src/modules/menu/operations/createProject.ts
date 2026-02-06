348442ae01446198664b7bd8aa257fe372b810aa
Ale Sanchez <alesanchezmedina@gmail.com>
2026-02-06 20:36:04 +0100

    fix(project): :bug: Create a project while having another opened (#123)

--- src/modules/menu/operations/createProject.ts
+++ src/modules/menu/operations/createProject.ts
@@ -3,28 +3,29 @@ import type { Board } from '../../board/context.svelte';
 import type { Config } from '../../config/context.svelte';
 import { createNewProject as createNewProjectModal } from '../../modalContent/addNewProject/createProject';
 import AddNewProjectModal from '../../modalContent/addNewProject/index.svelte';
+import type { Timeline } from '../../videoplayer/context.svelte';
 
-export async function createNewProject(board: Board, config: Config) {
+export async function createNewProject(board: Board, timeline: Timeline, config: Config) {
 	projectActions.setNewProjectFormData(null);
 	projectActions.setModal({
 		content: AddNewProjectModal,
 		title: 'Create New Project',
 		onCancel: handleCancel,
-		onSubmit: () => handleSubmit(board, config),
+		onSubmit: () => handleSubmit(board, timeline, config),
 		onSubmitText: 'Create',
 		show: true,
 		size: 'large'
 	});
 }
 
-async function handleSubmit(board: Board, config: Config) {
+async function handleSubmit(board: Board, timeline: Timeline, config: Config) {
 	const formData = projectActions.getNewProjectFormData();
 
 	if (!formData || !formData.projectPath) {
 		return;
 	}
 
-	await createNewProjectModal(board, config, formData.buttonBoard, formData.projectPath);
+	await createNewProjectModal(board, timeline, config, formData.buttonBoard, formData.projectPath);
 
 	if (formData.videoPath) {
 		await projectActions.setVideoPath(formData.videoPath);
