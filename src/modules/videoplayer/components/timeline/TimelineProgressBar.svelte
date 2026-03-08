5e06d3f8de8ea20ff8e228fad96b6b502640287e
Edith <eddith@proton.me>
2026-03-08 10:14:42 +0100

    feat: fix tag box and change time on drag

--- src/modules/videoplayer/components/timeline/TimelineProgressBar.svelte
+++ src/modules/videoplayer/components/timeline/TimelineProgressBar.svelte
@@ -168,6 +168,7 @@
 						{onEventDblClick}
 						{onEventResize}
 						{onEventContextMenu}
+						{onTimeChange}
 					/>
 				{/each}
 			{:else}
--- src/modules/videoplayer/components/video.svelte
+++ src/modules/videoplayer/components/video.svelte
@@ -22,7 +22,6 @@
 		currentTime = $bindable(),
 		duration = $bindable(),
 		playbackSpeed = 1.0,
-		// eslint-disable-next-line no-useless-assignment
 		videoPlayerRef = $bindable(),
 		onPlayStateChange,
 		onSkip,
